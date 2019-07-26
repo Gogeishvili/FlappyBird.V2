8906ce21f0ed6e4d084ba6e32c535ca86f50a573
giorgi <gio.gogeishvili@gmail.com>
2019-07-27 00:27:54 +0400

    Bird moving, pole moving , new pole

---
+++ Assets/CreateRandomPole.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRandomPole : MonoBehaviour
+{
+    public float maxTime = 1;
+    private float timer=0;
+    public GameObject Polle;
+    public float Height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newpole = Instantiate(Polle);
+        newpole.transform.position = transform.position + new Vector3(0, Random.Range(-Height, Height), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > maxTime)
+        {
+            GameObject newpole = Instantiate(Polle);
+            newpole.transform.position = transform.position + new Vector3(0, Random.Range(-Height, Height), 0);
+            Destroy(newpole, 15);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
---
+++ Assets/FLyBird.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FLyBird : MonoBehaviour
+{
+    public float Velocity = 1;
+    private Rigidbody2D RG;
+    void Start()
+    {
+        RG = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RG.velocity = Vector2.up * Velocity;
+        }
+    }
+}
